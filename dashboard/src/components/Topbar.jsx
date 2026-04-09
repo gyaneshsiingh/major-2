3489e53cde09a8d9b324da63acdf29f214a6398a
Gyanesh singh <gyanesh9875@gmail.com>
2026-04-09 13:45:50 +0530

    c

--- dashboard/src/components/Topbar.jsx
+++
@@ -1,8 +0,0 @@
-export default function Topbar({ onToggleSidebar }) {
-  return (
-    <header className="topbar">
-      <button className="mobile-menu-btn" onClick={onToggleSidebar}>☰</button>
-      <span className="topbar-title">Video Analytics Dashboard</span>
-    </header>
-  )
-}
